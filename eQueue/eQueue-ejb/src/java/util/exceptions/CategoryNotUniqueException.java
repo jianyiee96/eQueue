adae0da1fdbe67b8dc5df180f5a4539c4f2a35b9
thumster <bryan.thum@hotmail.com>
2020-02-26 11:49:05 +0800

    CategoryNotUniqueException

---
+++ eQueue/eQueue-ejb/src/java/util/exceptions/CategoryNotUniqueException.java
@@ -0,0 +1,11 @@
+package util.exceptions;
+
+public class CategoryNotUniqueException extends Exception {
+
+    public CategoryNotUniqueException() {
+    }
+
+    public CategoryNotUniqueException(String msg) {
+        super(msg);
+    }
+}
