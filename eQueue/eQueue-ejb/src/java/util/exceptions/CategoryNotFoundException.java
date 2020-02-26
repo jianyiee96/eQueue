d924524787e18f989f6f5a50ec18620626c54852
thumster <bryan.thum@hotmail.com>
2020-02-26 11:41:19 +0800

    Added CategoryNotFoundException

---
+++ eQueue/eQueue-ejb/src/java/util/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,11 @@
+package util.exceptions;
+
+public class CategoryNotFoundException extends Exception {
+
+    public CategoryNotFoundException() {
+    }
+
+    public CategoryNotFoundException(String msg) {
+        super(msg);
+    }
+}
