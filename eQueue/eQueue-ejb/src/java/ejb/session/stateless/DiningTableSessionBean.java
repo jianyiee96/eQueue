87637478b49a66a9e05a38bef398b97d4ff74b7e
JianYiee <hew1521@hotmail.com>
2020-03-21 19:29:25 +0800

    Added code generation codes

--- eQueue/eQueue-ejb/src/java/ejb/session/stateless/DiningTableSessionBean.java
+++ eQueue/eQueue-ejb/src/java/ejb/session/stateless/DiningTableSessionBean.java
@@ -2,8 +2,21 @@ package ejb.session.stateless;
 
 import entity.Customer;
 import entity.DiningTable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.io.File;
+import java.io.IOException;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -18,6 +31,7 @@ import util.exceptions.DeleteDiningTableException;
 import util.exceptions.DiningTableNotFoundException;
 import util.exceptions.EditTableException;
 import util.exceptions.InputDataValidationException;
+import util.exceptions.QrGenerationException;
 import util.exceptions.UnknownPersistenceException;
 
 @Stateless
@@ -100,6 +114,27 @@ public class DiningTableSessionBean implements DiningTableSessionBeanLocal {
     }
 
     @Override
+    private void generateQrCode(String code, String destinationPath) throws QrGenerationException {
+
+        try {
+            String qrCodeData = "www.chillyfacts.com";
+            String filePath = "D:\\QRCODE\\chillyfacts.png";
+            String charset = "UTF-8"; // or "ISO-8859-1"
+            Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<>();
+            hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+            BitMatrix matrix = new MultiFormatWriter().encode(
+                    new String(qrCodeData.getBytes(charset), charset),
+                    BarcodeFormat.QR_CODE, 200, 200, hintMap);
+            MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath
+                    .lastIndexOf('.') + 1), new File(filePath));
+            System.out.println("QR Code image created successfully!");
+        } catch (WriterException | IOException e) {
+            throw new QrGenerationException();
+        }
+
+    }
+
+    @Override
     public void deleteDiningTable(Long diningTableId) throws DiningTableNotFoundException, DeleteDiningTableException {
         DiningTable diningTableToRemove = retrieveDiningTableById(diningTableId);
 
@@ -120,7 +155,7 @@ public class DiningTableSessionBean implements DiningTableSessionBeanLocal {
 
     @Override // when customer did not seat at table in time or when customer leaves table
     public void removeCustomerTableRelationship(Long customerId) {
-        
+
         Customer customer = em.find(Customer.class, customerId);
         DiningTable diningTable = customer.getAllocatedDiningTable();
 
