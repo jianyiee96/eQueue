d1efee833e7852564d4ff77d0fc7d3005556cf4e
thumster <bryan.thum@hotmail.com>
2020-04-05 03:43:25 +0800

    Added overview

--- eQueue/eQueue-war/src/java/jsf/managedBean/KitchenManagementManagedBean.java
+++ eQueue/eQueue-war/src/java/jsf/managedBean/KitchenManagementManagedBean.java
@@ -1,18 +1,26 @@
 package jsf.managedBean;
 
 import ejb.session.stateless.CustomerOrderSessionBeanLocal;
+import ejb.session.stateless.MenuItemSessionBeanLocal;
 import ejb.session.stateless.OrderLineItemSessionBeanLocal;
 import entity.CustomerOrder;
+import entity.MenuItem;
 import entity.OrderLineItem;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.inject.Inject;
+import org.primefaces.PrimeFaces;
 import util.enumeration.OrderLineItemStatusEnum;
 import util.exceptions.CustomerOrderNotFoundException;
 import util.exceptions.InputDataValidationException;
@@ -27,9 +35,15 @@ public class KitchenManagementManagedBean implements Serializable {
     private CustomerOrderSessionBeanLocal customerOrderSessionBeanLocal;
     @EJB(name = "OrderLineItemSessionBeanLocal")
     private OrderLineItemSessionBeanLocal orderLineItemSessionBeanLocal;
+    @EJB
+    private MenuItemSessionBeanLocal menuItemSessionBean;
+    @Inject
+    private ViewMenuItemManagedBean viewMenuItemManagedBean;
 
     private List<CustomerOrder> currentDayCustomerOrders;
 //    private List<CustomerOrder> filteredCustomerOrders;
+    private List<Map.Entry<MenuItem, Integer>> menuItemsOverview;
+    private Map.Entry<MenuItem, Integer> menuItemToViewEntry;
 
     private OrderLineItem orderItemToView;
     private OrderLineItem orderItemToPrepare;
@@ -42,19 +56,42 @@ public class KitchenManagementManagedBean implements Serializable {
         orderItemToServe = new OrderLineItem();
     }
 
-    // To sort the current day order line items
-    public void sortCurrentDayOrderLineItems() {
+    @PostConstruct
+    public void postConstruct() {
         currentDayCustomerOrders = customerOrderSessionBeanLocal.retrieveCurrentDayOrders();
+        sortAllCurrentDayOrderLineItems();
         
-        for (int i = 0; currentDayCustomerOrders.size() > i; i++) {
-            List<OrderLineItem> items = currentDayCustomerOrders.get(i).getOrderLineItems();
-            Collections.sort(items, (i1, i2) -> i1.compareTo(i2));
+        createMenuItemsOverview();
+    }
+
+    // To sort the current day order line items
+    public void sortAllCurrentDayOrderLineItems() {
+        currentDayCustomerOrders.forEach(order -> {
+            sortOrderLineItems(order);
+        });
+    }
+
+    public void sortOrderLineItems(CustomerOrder order) {
+        Collections.sort(order.getOrderLineItems(), (i1, i2) -> i1.compareTo(i2));
+    }
+
+    public void createMenuItemsOverview() {
+        Map<MenuItem, Integer> map = new HashMap<>();
+        for (CustomerOrder customerOrder : currentDayCustomerOrders) {
+            for (OrderLineItem orderLineItem : customerOrder.getOrderLineItems()) {
+                MenuItem menuItem = orderLineItem.getMenuItem();
+                int count = map.containsKey(menuItem) ? map.get(menuItem) : 0;
+                map.put(menuItem, count + Math.toIntExact(orderLineItem.getQuantity()));
+            }
         }
+        menuItemsOverview = new ArrayList<>(map.entrySet());
     }
 
     public void prepare(ActionEvent event) {
         orderItemToPrepare = (OrderLineItem) event.getComponent().getAttributes().get("orderItemToPrepare");
+        CustomerOrder currentOrder = (CustomerOrder) event.getComponent().getAttributes().get("order");
         orderItemToPrepare.setStatus(OrderLineItemStatusEnum.PREPARING);
+        sortOrderLineItems(currentOrder);
         try {
             orderLineItemSessionBeanLocal.updateOrderLineItemByEmployee(orderItemToPrepare);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, orderItemToPrepare.getMenuItem().getMenuItemName() + " is being prepared.", null));
@@ -68,7 +105,9 @@ public class KitchenManagementManagedBean implements Serializable {
 
     public void serve(ActionEvent event) {
         orderItemToServe = (OrderLineItem) event.getComponent().getAttributes().get("orderItemToServe");
+        CustomerOrder currentOrder = (CustomerOrder) event.getComponent().getAttributes().get("order");
         orderItemToServe.setStatus(OrderLineItemStatusEnum.SERVED);
+        sortOrderLineItems(currentOrder);
         try {
             orderLineItemSessionBeanLocal.updateOrderLineItemByEmployee(orderItemToServe);
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, orderItemToServe.getMenuItem().getMenuItemName() + " is being served.", null));
@@ -94,8 +133,31 @@ public class KitchenManagementManagedBean implements Serializable {
         }
     }
 
+    public List<Map.Entry<MenuItem, Integer>> getMenuItemsOverview() {
+        return menuItemsOverview;
+    }
+
+    public void viewMenuItem() {
+        viewMenuItemManagedBean.setMenuItemToView(menuItemToViewEntry.getKey());
+    }
+
+    public Map.Entry<MenuItem, Integer> getMenuItemToViewEntry() {
+        return menuItemToViewEntry;
+    }
+
+    public void setMenuItemToViewEntry(Map.Entry<MenuItem, Integer> menuItemToViewEntry) {
+        this.menuItemToViewEntry = menuItemToViewEntry;
+    }
+
+    public ViewMenuItemManagedBean getViewMenuItemManagedBean() {
+        return viewMenuItemManagedBean;
+    }
+
+    public void setViewMenuItemManagedBean(ViewMenuItemManagedBean viewMenuItemManagedBean) {
+        this.viewMenuItemManagedBean = viewMenuItemManagedBean;
+    }
+
     public List<CustomerOrder> getCurrentDayCustomerOrders() {
-        sortCurrentDayOrderLineItems();
         return currentDayCustomerOrders;
     }
 
--- eQueue/eQueue-war/src/java/jsf/managedBean/ViewMenuItemManagedBean.java
+++ eQueue/eQueue-war/src/java/jsf/managedBean/ViewMenuItemManagedBean.java
@@ -1,8 +1,16 @@
 package jsf.managedBean;
 
 import entity.MenuItem;
+import java.io.File;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 
@@ -11,6 +19,7 @@ import javax.faces.view.ViewScoped;
 public class ViewMenuItemManagedBean implements Serializable {
 
     private MenuItem menuItemToView;
+    private String viewMenuItemCurrentPhotoContents;
 
     public ViewMenuItemManagedBean() {
         menuItemToView = new MenuItem();
@@ -20,12 +29,44 @@ public class ViewMenuItemManagedBean implements Serializable {
     public void postConstruct() {
     }
 
+    public String getViewMenuItemCurrentPhotoContents() {
+        return viewMenuItemCurrentPhotoContents;
+    }
+
     public MenuItem getMenuItemToView() {
         return menuItemToView;
     }
 
     public void setMenuItemToView(MenuItem menuItemToView) {
         this.menuItemToView = menuItemToView;
+        if (menuItemToView.getImagePath() != null) {
+            try {
+                viewMenuItemCurrentPhotoContents = getImageContentsAsBase64(Files.readAllBytes(getFileInDir(menuItemToView.getImagePath()).toPath()));
+            } catch (Exception ex) {
+                System.out.println("FILE DOES NOT EXIST! ===========> " + menuItemToView.getImagePath());
+            }
+        }
+    }
+
+    public String getImageContentsAsBase64(byte[] contents) {
+        return Base64.getEncoder().encodeToString(contents);
+    }
+
+    private File getFileInDir(String imagePath) {
+        String newFilePath = getWorkingDirPath() + imagePath;
+        File file = new File(newFilePath);
+        return file;
+    }
+
+    private String getWorkingDirPath() {
+        String newFilePath = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/");
+        Matcher m = Pattern.compile("eQueue").matcher(newFilePath);
+        List<Integer> positions = new ArrayList<>();
+        while (m.find()) {
+            positions.add(m.end());
+        }
+
+        return newFilePath.substring(0, positions.get(positions.size() - 3)) + "/eQueue-war/web/resources/images/food/";
     }
 
 }
