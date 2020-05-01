e1d17c83b23f0ad2923b6b095bc1770f1ebe95be
keithlim <keithlck96@gmail.com>
2020-05-01 17:35:23 +0800

    created logic to retrieve customer orders with transactionid, created logic to retrieve transactions with customerid

--- eQueue/eQueue-ejb/src/java/ejb/session/stateless/PaymentTransactionSessionBeanLocal.java
+++ eQueue/eQueue-ejb/src/java/ejb/session/stateless/PaymentTransactionSessionBeanLocal.java
@@ -1,6 +1,7 @@
 package ejb.session.stateless;
 
 import entity.PaymentTransaction;
+import java.util.List;
 import javax.ejb.Local;
 import util.exceptions.CustomerOrderNotFoundException;
 import util.exceptions.InputDataValidationException;
@@ -10,5 +11,7 @@ import util.exceptions.UnknownPersistenceException;
 public interface PaymentTransactionSessionBeanLocal {
 
     public Long createNewPaymentTransactionByCustomer(PaymentTransaction newPaymentTransaction) throws CustomerOrderNotFoundException, InputDataValidationException, UnknownPersistenceException;
+
+    public List<PaymentTransaction> retrievePaymentTransactions(Long customerId);
 
 }
